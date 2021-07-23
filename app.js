5855b097a94233cf6d4f02a024c9579578d28379
pavelsjo <pavels.jo@gmail.com>
2021-07-23 17:20:44 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// express app
+const app = express();
+app.listen(3000);
+
+// view engine
+app.set('view engine', 'ejs');
+
+// middlewire & static files
+app.use(express.json());
+app.use(express.static('public'));
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index', {title:'Intro'});
+});
+
+app.post('/', (req, res) => {
+
+    console.log(req.body);
+    res.send({"data":'Prediction'});
+    
+});
